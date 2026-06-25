4fa1b1ba5a38b28cfa7dc7d0379bac818718c4ff
shorforazkhan <shorforaz.bd@gmail.com>
2026-06-26 00:27:50 +0600

    1st git ignore commit

---
+++ abc.py
@@ -0,0 +1,9 @@
+# Python Code
+
+first_value = 33
+second_value = 30
+
+if first_value > second_value:
+    print(f"The larger value is {first_value}.")
+else:
+    print(f"The larger value is {second_value}.")
